6c8ed0a256be391e0e905e13f69675cf710a26e2
aditya <adityasingharyan007@gmail.com>
2024-11-25 14:46:13 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT= process.env.PORT ||4000;
+const port = process.env.PORT || 4000;
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
